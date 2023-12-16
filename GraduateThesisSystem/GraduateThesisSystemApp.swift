6dd894a50a71a06d056939aae5e0e446d8067bab
emirsafayavuz <emirsafayavuz@gmail.com>
2023-12-16 10:49:34 +0300

    Initial Commit

---
+++ GraduateThesisSystem/GraduateThesisSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraduateThesisSystemApp.swift
+//  GraduateThesisSystem
+//
+//  Created by Emir Safa Yavuz on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct GraduateThesisSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
